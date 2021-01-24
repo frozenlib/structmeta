a78121bcbb8099d633da25c3ec147bfcd0023015
frozenlib <frozenlib@users.noreply.github.com>
2021-01-25 01:29:43 +0900

    Add document.

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,14 @@ mod to_tokens_attribute;
 use syn::{parse_macro_input, DeriveInput};
 use syn_utils::*;
 
+/// Derive [`quote::ToTokens`].
 #[proc_macro_derive(ToTokens, attributes(to_tokens))]
 pub fn derive_to_tokens(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     into_macro_output(to_tokens::derive_to_tokens(input))
 }
 
+/// Derive [`syn::parse::Parse`].
 #[proc_macro_derive(Parse, attributes(to_tokens, parse))]
 pub fn derive_parse(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
