f452fcdbe9322e2ead7560d6df22010f8d39389e
frozenlib <frozenlib@users.noreply.github.com>
2021-03-29 13:00:47 +0900

    Improve error message.

--- structmeta/src/helpers.rs
+++ structmeta/src/helpers.rs
@@ -23,11 +23,7 @@ pub fn try_parse_name(
     no_unnamed: bool,
 ) -> Result<Option<(NameIndex, Span)>> {
     let fork = input.fork();
-    let ident = Ident::parse_any(&fork);
-    if no_unnamed {
-        ident.clone()?;
-    }
-    if let Ok(ident) = &ident {
+    if let Ok(ident) = Ident::parse_any(&fork) {
         let span = ident.span();
         let mut is_named = false;
         if (flag_map || !flag_names.is_empty()) && (fork.peek(Token![,]) || fork.is_empty()) {
@@ -55,16 +51,13 @@ pub fn try_parse_name(
         if is_named {
             let mut expected = Vec::new();
             if let Some(i) = find(flag_names, &ident) {
-                expected.push(format!("flag parameter `{}`", flag_names[i]));
+                expected.push(format!("`{}` (flag style parameter)", flag_names[i]));
             }
             if let Some(i) = find(name_value_names, &ident) {
-                expected.push(format!(
-                    "name value parameter `{} = ...`",
-                    name_value_names[i]
-                ));
+                expected.push(format!("`{} = ...`", name_value_names[i]));
             }
             if let Some(i) = find(name_args_names, &ident) {
-                expected.push(format!("name args parameter `{}(...)`", name_args_names[i]));
+                expected.push(format!("`{}(...)`", name_args_names[i]));
             }
             if !expected.is_empty() {
                 return Err(input.error(msg(&expected)));
@@ -73,6 +66,20 @@ pub fn try_parse_name(
         if is_named || no_unnamed {
             return Err(input.error(format!("cannot find parameter `{}` in this scope", ident)));
         }
+    }
+    if no_unnamed {
+        let message = if flag_names.is_empty()
+            && !flag_map
+            && name_value_names.is_empty()
+            && !name_value_map
+            && name_args_names.is_empty()
+            && !name_args_map
+        {
+            "too many arguments."
+        } else {
+            "too many unnamed arguments."
+        };
+        return Err(input.error(message));
     }
     Ok(None)
 }
@@ -109,6 +116,5 @@ fn msg(expected: &[String]) -> String {
             m.push_str(&expected[i]);
         }
     }
-    m.push('.');
     m
 }
